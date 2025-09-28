429ed291eb37f26de3a2dbd4eb9eed1a9f0755bd
ゆず <f.kokori85@gmail.com>
2025-09-28 09:39:05 +0900

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,9 @@
+// src/components/Layout.tsx
+
 import React from 'react';
 
-const Layout = ({ children }) => {
+// childrenの型を明示的に指定する
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="bg-gray-900 min-h-screen text-white">
       <header className="p-4 border-b border-gray-700 flex justify-between items-center">
